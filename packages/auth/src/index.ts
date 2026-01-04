493ccb09de4a86c95e5e662c52d1d55f1db67b5c
Mehdi Asadli <mehdiasadli@azericard.com>
2026-01-04 02:57:47 +0100

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,41 @@
+import prisma from "@encre/db";
+import { env } from "@encre/env/server";
+import { polar, checkout, portal } from "@polar-sh/better-auth";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { nextCookies } from "better-auth/next-js";
+
+import { polarClient } from "./lib/payments";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [
+    nextCookies(),
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      enableCustomerPortal: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "your-product-id",
+              slug: "pro",
+            },
+          ],
+          successUrl: env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true,
+        }),
+        portal(),
+      ],
+    }),
+    nextCookies(),
+  ],
+});
