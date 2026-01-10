3179fb5d335bc4ab26e4ac2037f1938ceaea18da
Mehdi Asadli <mehdiasadli@azericard.com>
2026-01-10 15:54:17 +0400

    feat: enhance feedback router with additional functionalities for responding, retrieving, updating, and deleting feedback

--- packages/api/src/modules/feedback/feedback.router.ts
+++ packages/api/src/modules/feedback/feedback.router.ts
@@ -1,6 +1,25 @@
-import { CreateFeedbackInputSchema } from "@encre/schemas/modules/feedback.schema";
-import { publicProcedure } from "../../procedures";
-import { createFeedback } from "./services/create-feedback";
+import {
+	AdminGetManyFeedbackInputSchema,
+	AdminGetManyFeedbackOutputSchema,
+	CreateFeedbackInputSchema,
+	DeleteFeedbackInputSchema,
+	GetManyFeedbackInputSchema,
+	GetManyFeedbackOutputSchema,
+	ResponseFeedbackInputSchema,
+	UpdateFeedbackInputSchema,
+} from "@encre/schemas/modules/feedback.schema";
+import {
+	moderatorOrAdminProcedure,
+	protectedProcedure,
+	publicProcedure,
+} from "../../procedures";
+import { createFeedback, respondToFeedback } from "./services/create-feedback";
+import { deleteFeedback } from "./services/delete-feedback";
+import {
+	adminGetManyFeedback,
+	getManyFeedback,
+} from "./services/get-many-feedback";
+import { updateFeedback } from "./services/update-feedback";
 
 export const feedbackRouter = {
 	createFeedback: publicProcedure
@@ -9,4 +28,27 @@ export const feedbackRouter = {
 			async ({ input, context }) =>
 				await createFeedback(input, context.user?.id),
 		),
+	respondToFeedback: moderatorOrAdminProcedure
+		.input(ResponseFeedbackInputSchema)
+		.handler(
+			async ({ input, context }) =>
+				await respondToFeedback(input, context.user.id),
+		),
+	getManyFeedback: protectedProcedure
+		.input(GetManyFeedbackInputSchema)
+		.output(GetManyFeedbackOutputSchema)
+		.handler(
+			async ({ input, context }) =>
+				await getManyFeedback(input, context.user.id),
+		),
+	adminGetManyFeedback: moderatorOrAdminProcedure
+		.input(AdminGetManyFeedbackInputSchema)
+		.output(AdminGetManyFeedbackOutputSchema)
+		.handler(async ({ input }) => await adminGetManyFeedback(input)),
+	updateFeedback: moderatorOrAdminProcedure
+		.input(UpdateFeedbackInputSchema)
+		.handler(async ({ input }) => await updateFeedback(input)),
+	deleteFeedback: moderatorOrAdminProcedure
+		.input(DeleteFeedbackInputSchema)
+		.handler(async ({ input }) => await deleteFeedback(input)),
 };
