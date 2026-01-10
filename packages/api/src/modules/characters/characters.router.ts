c1807660d7050090c2433ba8d899601b509d357d
mehdiasadli <asadlimehdi25@gmail.com>
2026-01-10 20:00:16 +0400

    feat: implement character router with create, get one, and get many character procedures

--- packages/api/src/modules/characters/characters.router.ts
+++ packages/api/src/modules/characters/characters.router.ts
@@ -1,1 +1,38 @@
-export const charactersRouter = {};
+import {
+	CreateCharacterInputSchema,
+	CreateCharacterOutputSchema,
+	GetCharacterInputSchema,
+	GetCharacterOutputSchema,
+	GetManyCharactersInputSchema,
+	GetManyCharactersOutputSchema,
+} from "@encre/schemas";
+import { authorProcedure, publicProcedure } from "../../procedures";
+import {
+	createCharacter,
+	getManyCharacters,
+	getOneCharacter,
+} from "./services";
+
+export const charactersRouter = {
+	createCharacter: authorProcedure
+		.input(CreateCharacterInputSchema)
+		.output(CreateCharacterOutputSchema)
+		.handler(
+			async ({ input, context }) =>
+				await createCharacter(input, context.author.id),
+		),
+	getOneCharacter: publicProcedure
+		.input(GetCharacterInputSchema)
+		.output(GetCharacterOutputSchema)
+		.handler(
+			async ({ input, context }) =>
+				await getOneCharacter(input, context.user?.id),
+		),
+	getManyCharacters: publicProcedure
+		.input(GetManyCharactersInputSchema)
+		.output(GetManyCharactersOutputSchema)
+		.handler(
+			async ({ input, context }) =>
+				await getManyCharacters(input, context.user?.id),
+		),
+};
