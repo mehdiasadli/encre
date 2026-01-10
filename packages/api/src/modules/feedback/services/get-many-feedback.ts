f4dc4de338cbf03592d146183d5cb10d7796b79b
Mehdi Asadli <mehdiasadli@azericard.com>
2026-01-10 16:04:31 +0400

    fix: exclude deleted feedbacks from retrieval in getManyFeedback service

--- packages/api/src/modules/feedback/services/get-many-feedback.ts
+++ packages/api/src/modules/feedback/services/get-many-feedback.ts
@@ -23,6 +23,7 @@ export async function getManyFeedback(
 
 	const where: Prisma.FeedbackWhereInput = {
 		userId,
+		status: { not: "deleted" },
 	};
 
 	if (input.statuses && input.statuses.length > 0) {
